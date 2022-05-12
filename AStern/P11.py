b926bb9b7f35d05e0a3a2d06c0e7b277fc7d2e3d
Christopher Feldhues <cf952789@fh-muenster.de>
2022-05-12 16:44:56 +0200

    A* läuft

--- AStern/P11.py
+++ AStern/P11.py
@@ -1,8 +1,9 @@
 from concurrent.futures import ProcessPoolExecutor
 import json
 from pprint import pprint
-from queue import Queue
+from queue import PriorityQueue, Queue
 import queue
+from tracemalloc import start
 from webbrowser import Konqueror
 
 
@@ -22,8 +23,13 @@ class Knoten():
     def add(self, nachbar):
         self.nachbarn.append(nachbar)
 
-
 class AStern():
+
+    def sucheKnoten(self, alleKnoten, zuSuchen: str):
+        for i in alleKnoten:
+            if i.name == zuSuchen:
+                return i
+        return None
 
     def calculateHeuristik(self, aktuellerKnoten, zielKnoten, alleKnoten):
         guenstigste = float("inf")
@@ -44,7 +50,6 @@ class AStern():
                     for p in k.nachbarn:
                         knoten.put((p, i[1]+1))
 
-
     def readJson(self):
         knoten = []
         with open("AStern/pfade.json") as file:
@@ -57,35 +62,57 @@ class AStern():
                 knoten.append(k)
         return knoten
 
-    # def calculate_end_note(self, board: Dict[str, str]):
-    #     node = PriorityEntry(0, board)
-    #     frontier = PriorityQueue()
-    #     frontier.put(node)
-    #     explored = set()
-    #
-    #     while True:
-    #         if frontier.empty():
-    #             print("Frontier is empty - Failure!")
-    #             exit(666)
-    #         node = frontier.get()
-    #         if Board.is_tie(node.data):
-    #             return node.data
-    #         explored.add(node)
-    #
-    #         playerChar = Board.get_player_char(node.data)
-    #         for i in Board.get_free_fields(node.data):
-    #             child_board = deepcopy(node.data)
-    #             child_board[i] = playerChar
-    #             child_path_cost = node.priority + self.get_path_cost(child_board)
-    #             child = PriorityEntry(child_path_cost, child_board)
-    #             if child not in explored or child not in frontier.queue:
-    #                 print(child_path_cost)
-    #                 frontier.put(child)
-    #             elif child in frontier.queue:
-    #                 for i in frontier.queue:
-    #                     if frontier.queue[i].data == child_board and frontier.queue[i].priority > child_path_cost:
-    #                         frontier.queue[i].priority = child_path_cost
+    def aStern(self, alleKnoten, startKnoten, zielKnoten):
+        # Zu besuchen
+        open = set([startKnoten])
+        # Schon besucht
+        closed = set([])
 
+        # Aktuelle Distanz vom Start zu all anderen Knoten
+        poo = {}
+        poo[startKnoten] = 0
+
+        # Von welchem Knoten komme ich?
+        par = {}
+        par[startKnoten] = startKnoten
+
+        while len(open) > 0:
+            n = None
+            # Geringsten Wert in der Liste finden
+            for v in open:
+                if n == None or poo[v] + self.calculateHeuristik(v, zielKnoten, alleKnoten) < poo[n] + self.calculateHeuristik(n, zielKnoten, alleKnoten):
+                    n = v
+            
+            if n == None:
+                return None
+            # Am Zielknoten angekommen
+            if n == zielKnoten:
+                path = []
+                while par[n] != n:
+                    path.append(n.name)
+                    n = par[n]
+                path.append(startKnoten.name)
+                path.reverse()
+                return path
+            # Für alle Nachbarn des aktuellen Knotens
+            for i in n.nachbarn:
+                knoten = self.sucheKnoten(alleKnoten, i.name)
+                # Wenn er noch nicht besucht wurde oder schon in der "zu besuchen" Liste steht
+                if knoten not in open and knoten not in closed:
+                    open.add(knoten)
+                    par[knoten] = n
+                    poo[knoten] = poo[n] + i.kosten
+                # Schauen, ob die Kosten vom vorherigen Besuch höher sind
+                else:
+                    if poo[knoten] > poo[n] + i.kosten:
+                        poo[knoten] = poo[n] + i.kosten
+                        par[knoten] = n
+                        # Nochmal besuchen
+                        if knoten in closed:
+                            closed.remove(knoten)
+                            open.add(knoten)
+            open.remove(n)
+            closed.add(n)
 
 if __name__ == "__main__":
     var = AStern()
@@ -93,8 +120,6 @@ if __name__ == "__main__":
     for i in knoten:
         pprint(i.name)
         for k in i.nachbarn:
-            pprint(f"Nachbar: {k.name}")
-    heu = var.calculateHeuristik(knoten[0], knoten[5], knoten)
-    print(heu)
-    #unPlayer = UniformCostSearchWithAStern()
-    #self.board = unPlayer.calculate_end_note(self.board)
+            pprint(f"--> Nachbar: {k.name} {k.kosten}")
+    print(var.aStern(knoten, knoten[0], knoten[5]))
+    
